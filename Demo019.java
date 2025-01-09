3820776b10f992e490f44428f26e2b72de2e289b
Mahesh-264 <maheshbonthala26@gmail.com>
2025-01-09 12:36:13 +0530

    Fibonacci series

--- Demo019.java
+++ Demo019.java
@@ -1,10 +1,16 @@
-import java.util.Scanner;
-class Demo019{
-    public static void main(String [] args){
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        System.out.println("Enter the number: "+a);
-        int rounded = (((a+2/5)/5)*5);
-        System.out.println("Rounded multiple of 5 is"+ rounded);
+public class Demo019b {
+    public static void main(String[] args) {
+        int num1 = 1;
+        int num2 = 2;
+        int sum = 0;
+
+        System.out.print("Fibonacci Sequence: " + num1 + ", " + num2);
+
+        for (int i = 3; i <= 10; i++) {
+            sum = num1 + num2;
+            num1 = num2;
+            num2 = sum;
+            System.out.print(", " + sum);
+        }
     }
 }
