723fa0d9000bd97a4d5b3318e59ad27b8f097e4f
Mahesh-264 <maheshbonthala26@gmail.com>
2025-01-09 12:36:46 +0530

    FizzBizz

--- Demo020.java
+++ Demo020.java
@@ -1,16 +1,22 @@
-public class Demo020 {
-    public static void main(String[] args) {
-        int num1 = 1;
-        int num2 = 2;
-        int sum = 0;
+import java.util.Scanner;
 
-        System.out.print("Fibonacci Sequence: " + num1 + ", " + num2);
-
-        for (int i = 3; i <= 10; i++) {
-            sum = num1 + num2;
-            num1 = num2;
-            num2 = sum;
-            System.out.print(", " + sum);
+class Demo020 {
+    public static String getFizzBuzz(int num) {
+        if (num % 3 == 0 && num % 5 == 0) {
+            return "FizzBuzz";
+        } else if (num % 3 == 0) {
+            return "Fizz";
+        } else if(num % 5 == 0) {
+            return "Buzz";
+        } else {
+            return String.valueOf(num);
         }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int i = sc.nextInt();
+        sc.close();
+        System.out.println(getFizzBuzz(i));
     }
 }
