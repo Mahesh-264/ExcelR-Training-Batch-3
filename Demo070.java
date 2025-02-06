4b1334efaa27fb4fda83883a4d0f2892fb5addf8
Mahesh-264 <maheshbonthala26@gmail.com>
2025-02-06 12:59:15 +0530

    Printing prime numbers in an array

--- Demo070.java
+++ Demo070.java
@@ -5,7 +5,7 @@ public class Demo070 {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the number of elements: ");
         int n = sc.nextInt();
-        int[] arr = new int[n];
+        int[] arr = new int[n]; 
         System.out.println("Enter " + n + " numbers:");
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
