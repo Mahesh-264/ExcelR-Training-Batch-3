1fd95833babd935673a027bff5018edaa13924cf
Mahesh-264 <maheshbonthala26@gmail.com>
2025-02-10 12:40:57 +0530

    Fibonacci series

--- Demo060.java
+++ Demo060.java
@@ -2,7 +2,7 @@ public class Demo060 {
     public static void main(String[] args) {
         System.out.println("Enter a Number to find fibonacci series: ");
         int n = 20;
-        System.out.println("Fibonacci series up to " + n + " terms:");
+        System.out.println("Fibonacci series up to " + n + " terms:"); 
         for (int i = 0; i < n; i++) {
             System.out.print(fibonacci(i) + " ");
         }
